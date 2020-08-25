8d4ff6848e234caf8e2a53e113d85899883b62de
MRMokwa <matheus.mokwa@gmail.com>
2020-08-25 10:05:43 -0300

    Add @angular/material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
+import { AppComponent } from './app.component';
 import { TabelaComponent } from './tabela/tabela.component';
 
 @NgModule({
   declarations: [AppComponent, TabelaComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
